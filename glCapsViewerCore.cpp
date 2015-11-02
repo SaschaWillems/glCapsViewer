b6351594966df250f46cde6d76e60d9167d47aaa
saschawillems <webmaster@saschawillems.de>
2015-11-02 21:26:41 +0100

    Clear internal format targets

--- glCapsViewerCore.cpp
+++ glCapsViewerCore.cpp
@@ -257,6 +257,7 @@ void glCapsViewerCore::readCompressedFormats()
 
 void glCapsViewerCore::readInternalFormats()
 {
+	internalFormatTargets.clear();
 	// TODO : Move to xml
 	GLenum targets[] = { GL_TEXTURE_1D, GL_TEXTURE_1D_ARRAY, GL_TEXTURE_2D, GL_TEXTURE_2D_ARRAY, GL_TEXTURE_3D, GL_TEXTURE_CUBE_MAP,
 		GL_TEXTURE_CUBE_MAP_ARRAY, GL_TEXTURE_RECTANGLE, GL_TEXTURE_BUFFER, GL_RENDERBUFFER, GL_TEXTURE_2D_MULTISAMPLE, GL_TEXTURE_2D_MULTISAMPLE_ARRAY };
